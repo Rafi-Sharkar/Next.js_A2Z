4169c367af6059832e2948899af7ac852f5f474c
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2025-01-18 13:26:29 +0600

    Image, font, Metadata, Fetching, Progressive Rendering, SSR, SSG, ISR

--- NextJS with JS/tut/app/about/mission/page.jsx
+++ NextJS with JS/tut/app/about/mission/page.jsx
@@ -1,10 +1,13 @@
 import Button from '@/app/components/Button';   // here @ is the root diractory
+import Image from 'next/image';
+import RS7 from '@/public/images/Rafi_Sharkar07.jpg'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla esse corrupti fugit quisquam? Exercitationem, non esse quae veniam qui deserunt?</p>
+      <Image src={RS7} alt='Image not found' placeholder='blur'/>
       <Button/>
     </div>
   )
